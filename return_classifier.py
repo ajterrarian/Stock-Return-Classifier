bb1dc26b94e48672879f21cabfa36d17a5d3473f
ajterrarian <199998374+ajterrarian@users.noreply.github.com>
2026-07-29 16:56:06 -0700

    Add naive baseline and logistic regression functions

--- return_classifier.py
+++ return_classifier.py
@@ -5,7 +5,10 @@ import yfinance as yf
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.preprocessing import StandardScaler
+import warnings
+warnings.filterwarnings('ignore', category=RuntimeWarning)
 
 def main():
     stock = input("Enter stock ticker (e.g., AAPL): ")
@@ -14,6 +17,10 @@ def main():
 
     data = load_and_clean_data(stock, start_date, end_date)
     X_train, X_test, y_train, y_test = split_features_and_labels(data, feature_cols = FEATURE_COLUMNS, label_col = 'Label', test_size = 0.2)
+
+    majority_position, y_pred_naive, naive_accuracy = naive_baseline(y_train, y_test)
+
+    model, y_pred, y_probability, accuracy = logistic_regression(X_train, X_test, y_train, y_test)
 
 
 def load_and_clean_data(stock, start_date, end_date):
@@ -40,6 +47,8 @@ def load_and_clean_data(stock, start_date, end_date):
 
     next_return = data['Daily Return'].shift(-1)
     data['Label'] = (next_return > 0).where(next_return.notna())
+
+    data = data.replace([np.inf, -np.inf], np.nan)
     data = data.dropna()
     data['Label'] = data['Label'].astype(int)
 
@@ -54,6 +63,52 @@ def split_features_and_labels(data, feature_cols = FEATURE_COLUMNS, label_col = 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_size, shuffle = False)
 
     return X_train, X_test, y_train, y_test
+
+def naive_baseline(y_train, y_test):
+    #find majority position and flip all values to match
+    majority_position = y_train.value_counts().idxmax()
+    y_pred_naive = pd.Series(majority_position, index = y_test.index, name = 'Naive Position')
+
+    #test accuracy
+    naive_accuracy = accuracy_score(y_test, y_pred_naive)
+    print(f"Accuracy score: {naive_accuracy:.4f}\n")
+
+    report_dict = classification_report(y_test, y_pred_naive, zero_division=0, output_dict=True)
+    report_df = pd.DataFrame(report_dict).transpose()
+    report_df.iloc[:, :3] = report_df.iloc[:, :3].replace(0.0, np.nan) # Replace precision, recall, f1
+    print(report_df.to_string())
+
+    print("Baseline Confusion Matrix:")
+    print(confusion_matrix(y_test, y_pred_naive))
+
+    return majority_position, y_pred_naive, naive_accuracy
+
+def logistic_regression(X_train, X_test, y_train, y_test):
+    #scale features
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    #start and train the model
+    model = LogisticRegression(solver = 'lbfgs', max_iter = 1000)
+    model.fit(X_train_scaled, y_train)
+
+    #make predictions on the test set
+    y_pred = model.predict(X_test_scaled)
+    y_probability = model.predict_proba(X_test_scaled)
+
+    #evaluate performance
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Accuracy score: {accuracy:.4f}\n")
+    report_dict = classification_report(y_test, y_pred, zero_division=0, output_dict=True)
+    report_df = pd.DataFrame(report_dict).transpose()
+    report_df.iloc[:, :3] = report_df.iloc[:, :3].replace(0.0, np.nan) # Replace precision, recall, f1
+    print(report_df.to_string())
+    print("Confusion Matrix:")
+    print(confusion_matrix(y_test, y_pred))
+
+    return model, y_pred, y_probability, accuracy
+
 
 if __name__ == "__main__":
     main()
