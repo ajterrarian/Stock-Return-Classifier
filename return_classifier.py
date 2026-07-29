62f2585bbaf7f5fd6e72fc64a05b9dc272cd4adc
ajterrarian <ajterrarian@gmail.com>
2026-07-29 09:12:31 -0700

    Initial commit

---
+++ return_classifier.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
